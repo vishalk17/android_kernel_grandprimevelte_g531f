0c955c399ad5d11d09f1b43ac1fa80f4cba0a0fd
vishalk17 <vishalkapadi95@gmail.com>
2018-02-16 11:06:43 +0000

    sched: fix compilation errors:add prio.h
    errors were,
    
    /root/los15/kernel/leeco/x3/drivers/android/binder.c: In function 'to_userspace_prio':
    /root/los15/kernel/leeco/x3/drivers/android/binder.c:1099:3: error: implicit declaration of function 'PRIO_TO_NICE' [-Werror=implicit-function-declaration]
       return PRIO_TO_NICE(kernel_priority);
       ^
    /root/los15/kernel/leeco/x3/drivers/android/binder.c:1101:10: error: 'MAX_USER_RT_PRIO' undeclared (first use in this function)
       return MAX_USER_RT_PRIO - 1 - kernel_priority;
              ^
    /root/los15/kernel/leeco/x3/drivers/android/binder.c:1101:10: note: each undeclared identifier is reported only once for each function it appears in
    /root/los15/kernel/leeco/x3/drivers/android/binder.c: In function 'to_kernel_prio':
    /root/los15/kernel/leeco/x3/drivers/android/binder.c:1107:3: error: implicit declaration of function 'NICE_TO_PRIO' [-Werror=implicit-function-declaration]
       return NICE_TO_PRIO(user_priority);
       ^
    /root/los15/kernel/leeco/x3/drivers/android/binder.c:1109:10: error: 'MAX_USER_RT_PRIO' undeclared (first use in this function)
       return MAX_USER_RT_PRIO - 1 - user_priority;
              ^
    /root/los15/kernel/leeco/x3/drivers/android/binder.c: In function 'binder_do_set_priority':
    /root/los15/kernel/leeco/x3/drivers/android/binder.c:1132:15: error: 'MIN_NICE' undeclared (first use in this function)
        priority = MIN_NICE;
                   ^
    /root/los15/kernel/leeco/x3/drivers/android/binder.c:1139:3: error: implicit declaration of function 'rlimit_to_nice' [-Werror=implicit-function-declaration]
       long min_nice = rlimit_to_nice(task_rlimit(task, RLIMIT_NICE));
       ^
    /root/los15/kernel/leeco/x3/drivers/android/binder.c:1141:18: error: 'MAX_NICE' undeclared (first use in this function)
       if (min_nice > MAX_NICE) {
    
    Signed-off-by: vishal kapadi <vishalkapadi95@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -28,8 +28,9 @@ extern void update_cpu_load_active(struct rq *this_rq);
  * to static priority [ MAX_RT_PRIO..MAX_PRIO-1 ],
  * and back.
  */
-#define NICE_TO_PRIO(nice)	(MAX_RT_PRIO + (nice) + 20)
-#define PRIO_TO_NICE(prio)	((prio) - MAX_RT_PRIO - 20)
+
+//#define NICE_TO_PRIO(nice)	(MAX_RT_PRIO + (nice) + 20)
+//#define PRIO_TO_NICE(prio)	((prio) - MAX_RT_PRIO - 20)
 #define TASK_NICE(p)		PRIO_TO_NICE((p)->static_prio)
 
 /*
